fb57b014b5f6da4a4e4c8f14b6319a2d7edf8adf
João Pedro <fjoaopedro1302@gmail.com>
2026-01-02 12:11:04 -0300

    refactor: funções prng movidas para um novo arquivo, seguindo o modelo de responsabilidade única

--- src/core/dailySeed.ts
+++ src/core/dailySeed.ts
@@ -9,16 +9,3 @@ export function generateSeed(date?: Date | null): number { // Função para gera
 
     return parseInt(formattedDate);
 }
-
-export function prng(seed: number) : Function { // Função do Geradorade Números Pseudoaleatórios (PRNG) usando método de Congruência Linear (LCG)
-    const A: number = 31;
-    const B: number = 23;
-    const M: number = 1000000007;
-
-    seed = ((seed * A) + B) % M;
-
-    return function next(): number {
-        seed = ((seed * A) + B) % M;
-        return seed;
-    }
-}
