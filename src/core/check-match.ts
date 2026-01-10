6a36a768ef3ece07c076419c083aedb01795f09c
João Pedro <fjoaopedro1302@gmail.com>
2026-01-10 14:12:33 -0300

    fix: fallback para evitar comparação entre cartas potencialmente nulas desenvolvido

--- src/core/check-match.ts
+++ src/core/check-match.ts
@@ -1,5 +1,9 @@
 import type { Card } from "../types";
 
 export function checkMatch(card1: Card, card2: Card): boolean {
+    if (!card1 ||!card2) {
+        return false;
+    }
+
     return card1.name === card2.name && card1.id !== card2.id;
 }   
